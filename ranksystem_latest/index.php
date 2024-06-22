c39bea1c2f87c07f1851d865df9d8e771461dc39
lachysek <127331280+lachysek@users.noreply.github.com>
2024-06-22 19:58:00 +0200

    Updated

--- ranksystem_latest/index.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$target = $_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['PHP_SELF']), '/\\').'/stats/';
-if(isset($_SERVER['HTTPS'])) {
-	header('Location: https://'.$target);
-} else {
-	header('Location: http://'.$target);
-}
-?>
